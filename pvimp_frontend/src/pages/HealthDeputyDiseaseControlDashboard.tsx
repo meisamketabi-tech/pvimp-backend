181d54385616709392cd8c2fed44203c4bad6086
meisamketabi-tech <meisam.ketabi@gmail.com>
2026-08-20 01:31:01 +0330

    feat: add health deputy disease control view

---
+++ pvimp_frontend/src/pages/HealthDeputyDiseaseControlDashboard.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DiseaseControlManagementDashboard from "./DiseaseControlManagementDashboard";
+
+export default function HealthDeputyDiseaseControlDashboard() {
+  return <DiseaseControlManagementDashboard title="داشبورد معاونت سلامت — مبارزه با بیماری‌های دامی" />;
+}
