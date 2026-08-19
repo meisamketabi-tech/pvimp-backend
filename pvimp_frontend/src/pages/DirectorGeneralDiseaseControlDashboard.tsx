92af0687a8ae829d3c9f72229675aec51f0fc2cc
meisamketabi-tech <meisam.ketabi@gmail.com>
2026-08-20 01:31:06 +0330

    feat: add director general disease control view

---
+++ pvimp_frontend/src/pages/DirectorGeneralDiseaseControlDashboard.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DiseaseControlManagementDashboard from "./DiseaseControlManagementDashboard";
+
+export default function DirectorGeneralDiseaseControlDashboard() {
+  return <DiseaseControlManagementDashboard title="داشبورد مدیرکل — مبارزه با بیماری‌های دامی" />;
+}
